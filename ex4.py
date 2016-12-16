c50cbf47baf21c766c1234b6d17bd75d9cdeaf92
karamarimo <admin@h168.249.229.10.1017421.vlan.kuins.net>
2016-12-16 14:32:14 +0900

    add general image loader using PIL

--- ex4.py
+++ ex4.py
@@ -1,7 +1,17 @@
-from QR import loadImage, readQRImage
+from QR import loadImage, readQRImage, stringToQRImage, saveImage
+import ImageLoader
 
-filename = 'qr7.ppm'
-image = loadImage(filename)
-decoded = readQRImage(image)
-print(decoded)
-# QR.saveImage(bimage, "binary_" + filename)
+# filename = 'qr3.ppm'
+# image = loadImage(filename)
+# decoded = readQRImage(image)
+# print(decoded)
+
+filename2 = 'QRsample.jpg'
+im = ImageLoader.loadImage(filename2)
+s = readQRImage(im, strict=True)
+print(s.encode('utf-8'))
+# saveImage(stringToQRImage(s), 'testtt.ppm')
+
+# tl (190.05555555555554, 377.8888888888889)
+# bl (338.42, 356.0)
+# tr (169.98076923076923, 225.6153846153846)
